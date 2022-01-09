79a0ac6080e584f3a60d8369e36786449af78790
fserz <873444264@qq.com>
2022-01-09 23:04:28 +0800

    quick sort

--- 1045.cpp
+++ 1045.cpp
@@ -1,21 +1,37 @@
-# include <cstdio>
-# include <algorithm>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
-
+int v[100010];
 
 
 int main(){
-    int N = 0x7fffffff;
-    /*scanf("%d", &N);
-    for(i = 0; i < n; i++){
-
-    }*/
-    printf("%d", N);
-
-
-
-
-
-
+    int n, max = 0, count = 0;
+    scanf("%d", &n);
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        b[i] = a[i];
+    };
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == b[i] && b[i] > max)
+        {
+            v[count] = b[i];
+            count++;
+        }
+        if (b[i] > max)
+            max = b[i];
+    }
+    cout << count << endl;
+   for (int i = 0; i < count; i++)
+   {
+           if (i != 0)
+           cout << " ";
+            cout << v[i];
+   }
+   cout << endl;
     return 0;
 }
