9c5629a222003035d8903024c53425cfafc5dbbd
fserz <873444264@qq.com>
2022-01-14 21:36:47 +0800

    待修改

---
+++ 1061.cpp
@@ -0,0 +1,20 @@
+# include <cstdio>
+# include <iostream>
+# include <vector>
+using namespace std:
+
+
+int main(){
+int N, M;
+cin >> N >> M;
+vector<int> a(M);
+for (int i = 0; i < M; i++)
+{
+    cin >> a(i);
+}
+
+
+
+
+    return 0;
+}
