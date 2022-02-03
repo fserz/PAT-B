ba68f783cba4f4d51dd927432db79b7784316e7b
fserz <873444264@qq.com>
2022-02-03 22:11:34 +0800

    1005

--- 1005.cpp
+++ 1005.cpp
@@ -1,54 +1,40 @@
-# include <cstdio>
-# include <cstring>
+# include <iostream>
 # include <algorithm>
+# include <vector>
 using namespace std;
-bool Hashtable[10000] = {false};
-bool cmp(int a, int b){
+int arr[10000];
+bool cmp(int a, int b)
+{
     return a > b;
 }
-
-
-
-int main(){
-int k, n, a[110];
-scanf("%d", &k);
-for(int i = 0; i < n; i++){
-scanf("%d", &a[i]);
-n = a[i];
- while(n != 1){
-     if (n % 2 == 1)
-     {
-         n = (3 * n + 1) / 2;
-     }
-     else
-     {
-         n = n / 2;
-     }
-     Hashtable[n] = true;
-        }
-}
-
-
-
-int count = 0;
-for(int i = 0; i < k; i++){
-    if(Hashtable[a[i]] == false)
-    count++;
-}
-
-
-sort(a, a + k, cmp);
-for(int i = 0; i < n; i++){
-    if (Hashtable[a[i]] == false){
-        printf("%d", a[i]);
-        count--;
-        if(count > 0){
-            printf(" ");
+int main (){
+    int n, k, flag = 0;
+    cin >> k;
+    vector<int> v(k);
+    for (int i = 0; i < k; i++)
+    {
+        cin >> n;
+        v[i] = n;
+        while (n != 1)
+        {
+            if (n % 2 != 0)
+                n = (3 * n + 1) / 2;
+            else
+                n /= 2;
+            if (arr[n] == 1)//已出现过故break
+            break;
+            arr[n] = 1;//未出现过的将其对应位置为1
         }
     }
-}
-
-
-
+    sort(v.begin(), v.end(), cmp);
+    for (int  i = 0; i < v.size(); i++)
+    {
+        if (arr[v[i]] == 0){//输出没有被覆盖的数
+            if (flag == 1)
+            cout << " ";
+            cout << v[i];
+            flag = 1;
+        }
+    }
     return 0;
 }
