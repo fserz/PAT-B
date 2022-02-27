ca674d74ba36311dd5cb7eccb761b8b736c4b44e
fserz <873444264@qq.com>
2022-02-27 23:18:20 +0800

    1035

--- 1035.cpp
+++ 1035.cpp
@@ -1,67 +1,37 @@
-# include <cstdio>
+# include <iostream>
 # include <algorithm>
 using namespace std;
-const n = 120;
-int origin[n], changed[n], temo[n];
-int n;
-
-bool isSame(int A[], int B[]){
-    for(int i =0; i < n; i++){
-        if(A[i] != B[i]{
-        return false
-    }
-         return ture;
-}
-
-bool showArray(int A[]){
-    for(int i = 0; i < n; i++){
-        printf("%d", &A[i]);
-        if(i < n - 1)
-        printf(" ");
-    }
-    printf("\n");
-}
-
-bool insetSort(){
-    bool flag = false;
-    for(int i = 1; i < n; i++){
-        if(i != 1 && isSame(temo, changed)){
-            flag = true;
-        }  
-        int temp = temo[i], j = 1;
-        while(j > 0 && temo[j - 1] > temp){
-            temo[j] = temo[j - 1];
-            j--;
-        }
-        temo[j] = temp;
-        if(flag == true){
-            return true;
-        }                        
-    }
-    return false;
-}
-
-
-int main() {
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++){
-    temo[i] = origin[i];
-    }
-    for(int i = 0; i < n; i++){
-        scanf("%d", &changed[i]);        
-    }
-    if(){
-        printf("Insertion Sort\n");
-        showArray(temo);
-    }else
+int main(){
+    int a[100], b[100], n, i, j;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for(int i = 0; i < n; i++)
+        cin >> b[i];
+    for ( i = 0; i < n - 1 && b[i] <= b[i + 1]; i++);//没有循环体
+    for (j = i + 1; a[j] == b[j] && j < n; j++);//没有循环体
+    if (j == n)
     {
-        printf("Merge Sort\n");
-
+        cout << "Insertion Sort" << endl;
+        sort(a, a + i + 2);
+    }else{
+        cout << "Merge Sort" << endl;
+        int k = 1, flag = 1;
+        while (flag){
+            flag = 0;
+            for (int i = 0; i < n; i++){
+                if (a[i] != b[i])
+                    flag = 1;
+                }
+                k *= 2;
+                for (i = 0; i < n / k; i++)
+                    sort(a + i * k, a + (i + 1) * k);
+                sort(a + n / k * k, a + n);
+            }
+        }
+        for (int j = 0; j < n; j++){
+            if( j != 0) cout << " ";
+            cout << a[j];
     }
-    
-
-
-    return 0；
+    return 0;
 }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-
