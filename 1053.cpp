e66017f7e64381dfd468669b8eeff3c29407ec24
fserz <873444264@qq.com>
2022-03-03 00:11:17 +0800

    1053

---
+++ 1053.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int n, d, k, maybe = 0, must = 0;
+    double e, temp;
+    cin >> n >> e >> d;
+    for (int i = 0; i < n; i++)
+    {
+        int day = 0;
+        cin >> k;
+        for (int j = 0; j < k; j++)
+        {
+            cin >> temp;
+            if (temp < e)
+                day++;  
+        }
+        if (day > (k / 2)){
+            k > d ? must++ : maybe++;//must和maybe不会重叠，属于msybe则不属于must
+            }
+    }
+    double mayberate = (double) maybe / n *100;
+    double mustrate = (double) must / n *100;
+    printf("%.1f%% %.1f%%", mayberate, mustrate);//输出%要使用（两个）%%
+    return 0;
+} 
